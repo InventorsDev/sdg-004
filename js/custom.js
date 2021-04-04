01136a0c1af1a9cfc9e52e872e5e38fd503a6fde
Onabajo Oluwakeji Daniel <62175369+OnabajoOluwakeji@users.noreply.github.com>
2021-04-04 21:30:00 +0100

    Project Setup
    This file contains the base unit for future tasks

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+
+  $(function () {
+
+    // MENU
+    $('.nav-link').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+
+    // AOS ANIMATION
+    AOS.init({
+      disable: 'mobile',
+      duration: 800,
+      anchorPlacement: 'center-bottom'
+    });
+
+
+    // SMOOTH SCROLL
+    $(function() {
+      $('.nav-link').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 0
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+    
+    // FEATURED SLIDE
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 30,
+      responsiveClass:true,
+      responsive:{
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1000: {
+          items: 3
+        }
+      }
+    });
+
+  });
